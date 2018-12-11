5858a36d2fe633307f3c3e74bc41a53bb3752d1c
Adam Wolfe <mlbfan2@gmail.com>
2018-12-11 16:04:02 -0500

    database creation

--- mealplanner.sql
+++ mealplanner.sql
@@ -1,33 +1,85 @@
-﻿
--- Switch to the system (aka master) database
-USE master;
-GO
-
--- Delete the DemoDB Database (IF EXISTS)
-IF EXISTS(select * from sys.databases where name='DemoDB')
-DROP DATABASE DemoDB;
-GO
-
--- Create a new DemoDB Database
-CREATE DATABASE DemoDB;
-GO
-
--- Switch to the DemoDB Database
-USE MealPlanner
-GO
+USE MealPlanner;
 
 BEGIN TRANSACTION;
 
-CREATE TABLE users
+CREATE TABLE ingredients
 (
-	id			int			identity(1,1),
-	username	varchar(50)	not null,
-	password	varchar(50)	not null,
-	salt		varchar(50)	not null,
-	role		varchar(50)	default('user'),
+	ingredientId int IDENTITY(1,1) NOT NULL,
+	ingredientName varchar(max) NOT NULL,
+	ingredientImage varchar(max) NULL,
 
-	constraint pk_users primary key (id)
+	constraint pk_ingredientId primary key (ingredientId)
 );
 
+CREATE TABLE groceryLists
+(
+	groceryListId int IDENTITY(1,1) NOT NULL,
+	ingredientId int NOT NULL,
+	item varchar(50) NOT NULL,
+	groceryListImage varchar(50) NULL,
+	quantity int NULL,
+
+	constraint pk_groceryListId primary key(groceryListId),
+	--constraint fk_ingredientId foreign key(ingredientId) references ingredients (ingredientId)
+);
+
+CREATE TABLE recipes
+(
+	recipeId int IDENTITY(1,1) NOT NULL,
+	ingredientId int NOT NULL,
+	userId int NOT NULL,
+	recipeDescription varchar(max) NULL,
+	recipeName varchar(50) NULL,
+	instructions varchar(max) NULL,
+	cookTime int NULL,
+	prepTime int NULL,
+	recipeType varchar(50) NULL,
+	gluten bit NULL,
+	vegetarian bit NULL,
+	dairy bit NULL,
+	nuts bit NULL,
+	vegan bit NULL,
+	servings int NULL,
+	caloriesPerServing int NULL,
+	fat float NULL,
+	carbohydrates float NULL,
+	protein float NULL,
+	fiber float NULL,
+	cholesterol float NULL,
+	sodium float NULL,
+	recipeImage varchar(max) NULL,
+
+	constraint pk_recipeId primary key(recipeId),
+	constraint fk_ingredientId foreign key(ingredientId) references ingredients (ingredientId)
+);
+
+CREATE TABLE mealPlans
+(
+	mealPlanId int IDENTITY(1,1) NOT NULL,
+	recipeId int NOT NULL,
+	mealPlanName varchar(50) NOT NULL,
+	mealPlanImage varchar(50) NULL,
+	recipeCount int NOT NULL,
+
+	constraint pk_mealPlanId primary key(mealPlanId), 
+	constraint fk_recipeId foreign key(recipeId) references recipes (recipeId)
+);
+
+CREATE TABLE users
+(
+	userId int IDENTITY(1,1) NOT NULL,
+	recipeId int NOT NULL,
+	mealPlanId int NOT NULL,
+	groceryListId int NOT NULL,
+	username varchar(50) NOT NULL,
+	password varchar(50) NOT NULL,
+	salt varchar(50) NOT NULL,
+	role varchar(50) default('user'),
+
+	constraint pk_userId primary key (userId),
+	--constraint fk_recipeId foreign key (recipeId) references recipes (recipeId),
+	constraint fk_mealPlanId foreign key (mealPlanId) references mealPlans (mealPlanId),
+	constraint fk_groceryListId foreign key (groceryListId) references groceryLists (groceryListId)
+);
 
 COMMIT TRANSACTION;
