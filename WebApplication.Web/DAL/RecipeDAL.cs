4836ea892e79125d29c88f59cfc8897c86910f14
Vincent Russo <Vincent Russo@TE-0CV101361137>
2018-12-11 14:03:40 -0500

    pushing

--- WebApplication.Web/DAL/RecipeDAL.cs
+++ WebApplication.Web/DAL/RecipeDAL.cs
@@ -14,30 +14,30 @@ namespace WebApplication.Web.DAL
         const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MealPlanner;Integrated Security=True";
         const string AddRecipeString = " INSERT INTO recipes(description, name, instructions, cookTime, prepTime, recipeType, gluten, vegetarian, dairy, nuts, vegan, servings, caloriesPerServing, fat, carbohydrates, protein, fiber, cholesterol, sodium) "+
             "values(@description, @name, @instructions, @cookTime, @prepTime, @recipeType, @gluten, @vegetarian, @dairy, @nuts, @vegan, @servings, @caloriesPerServing, @fat, @carbohydrates, @protein, @fiber, @cholesterol, @sodium)";
-
+        const string RecipeListTop5String = "Select * from recipes";
         public Recipes dapperDemoRecipe = new Recipes();
 
         public int addRecipes(Recipes newRecipe)
         {
-            dapperDemoRecipe.Name = "Pancakes";
-            dapperDemoRecipe.Description = "How to make some Gluten Free Pancakes that adam doesnt feel like eating";
-            dapperDemoRecipe.Instructions = "Adam Brings in the mix and we read the package";
-            dapperDemoRecipe.CookTime = 20;
-            dapperDemoRecipe.PrepTime = 10;
-            dapperDemoRecipe.RecipeType = "breakfast";
-            dapperDemoRecipe.Gluten = false;
-            dapperDemoRecipe.Vegetarian = true;
-            dapperDemoRecipe.Dairy = true;
-            dapperDemoRecipe.Nuts = false;
-            dapperDemoRecipe.Vegan = false;
-            dapperDemoRecipe.Servings = 4;
-            dapperDemoRecipe.CaloriesPerServing = 300;
-            dapperDemoRecipe.Fat = 4;
-            dapperDemoRecipe.Carbohydrates = 39;
-            dapperDemoRecipe.Protein = 5;
-            dapperDemoRecipe.Fiber = 2;
-            dapperDemoRecipe.Cholesterol = 10;
-            dapperDemoRecipe.Sodium = 1;
+            //dapperDemoRecipe.Name = "Pancakes";
+            //dapperDemoRecipe.Description = "How to make some Gluten Free Pancakes that adam doesnt feel like eating";
+            //dapperDemoRecipe.Instructions = "Adam Brings in the mix and we read the package";
+            //dapperDemoRecipe.CookTime = 20;
+            //dapperDemoRecipe.PrepTime = 10;
+            //dapperDemoRecipe.RecipeType = "breakfast";
+            //dapperDemoRecipe.Gluten = false;
+            //dapperDemoRecipe.Vegetarian = true;
+            //dapperDemoRecipe.Dairy = true;
+            //dapperDemoRecipe.Nuts = false;
+            //dapperDemoRecipe.Vegan = false;
+            //dapperDemoRecipe.Servings = 4;
+            //dapperDemoRecipe.CaloriesPerServing = 300;
+            //dapperDemoRecipe.Fat = 4;
+            //dapperDemoRecipe.Carbohydrates = 39;
+            //dapperDemoRecipe.Protein = 5;
+            //dapperDemoRecipe.Fiber = 2;
+            //dapperDemoRecipe.Cholesterol = 10;
+            //dapperDemoRecipe.Sodium = 1;
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -45,6 +45,16 @@ namespace WebApplication.Web.DAL
                 int affectRows = connection.Execute(AddRecipeString, dapperDemoRecipe);
                 return affectRows;
 
+            }
+
+        }
+        public List<Recipes> recipesTop5()
+        {
+            using (SqlConnection connection = new SqlConnection(RecipeListTop5String))
+            {
+                connection.Open();
+                List<Recipes> result = connection.Query<Recipes>(RecipeListTop5String).ToList();
+                return result;
             }
         }
         
