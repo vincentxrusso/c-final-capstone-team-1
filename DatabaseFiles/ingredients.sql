8608a4f9a4b1e52d1efc9ecd00e01103ac015101
Adam Wolfe <mlbfan2@gmail.com>
2018-12-12 18:35:38 -0500

    added ingredients into the database

---
+++ DatabaseFiles/ingredients.sql
@@ -0,0 +1,139 @@
+use MealPlanner;
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Rice Flour', 'https://www.bobsredmill.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/4/1460c244_brownriceflour_f_1800.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Tapioca Flour', 'http://kdlfoods.com/image/cache/data/Bobs-Red-Mill-Tapioca-Flour-039978025357-500x500.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Potato Starch', 'https://www.bobsredmill.com/media/catalog/product/cache/1/thumbnail/670x630/9df78eab33525d08d6e5fb8d27136e95/1/4/1444s224_potatostarch_f_hr_1.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Sweet Cream Buttermilk Powder', 'https://www.bobsredmill.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/2/1290c244_sweetcreambuttermilkpowder_f_1800.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Sugar', 'https://dcassetcdn.com/design_img/900074/153037/153037_5172790_900074_image.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Sugar Substitute', 'https://livethelive.files.wordpress.com/2014/03/sweeteners.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Baking Powder', 'https://target.scene7.com/is/image/Target/GUEST_e227abc0-03c6-45dd-a41d-bf47f4a9741d?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Baking Soda', 'https://pics.drugstore.com/prodimg/195585/900.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Salt', 'http://pauseforthought.com/wp-content/uploads/2015/04/PFT_MORTON_11.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Xanthan Gum', 'https://www.bobsredmill.com/media/catalog/product/cache/1/thumbnail/670x630/9df78eab33525d08d6e5fb8d27136e95/1/5/1599s086_xanthangum_f_hr_1.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Eggs', 'https://www.drweil.com/wp-content/uploads/2015/11/diet-nutrition_food-safety_are-pastured-eggs-better_88518085-1024x768.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Canola Oil', 'https://www.londondrugs.com/on/demandware.static/-/Sites-londondrugs-master/default/dw7eb8df1b/products/L2263267/large/L2263267.JPG');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Water', 'https://cdn.trendhunterstatic.com/thumbs/kitchen-water-faucets.jpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Flaked Coconut', 'https://images-na.ssl-images-amazon.com/images/I/71a-Hbp21kL._SL1500_.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Chopped Walnuts', 'https://i5.walmartimages.com/asr/eb02802f-d351-4112-a55d-7ce5aa3f9a9c_1.24a3228e6de090ce5f80c8d75d6cdeea.jpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Coconut Milk', 'http://cdn.shopify.com/s/files/1/2603/1874/products/b4e2d537fdb4a65f1033ba7cd062f692_e7170239-804c-4460-917b-8089fed33119_1024x1024.jpg?v=1530121071');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Olive Oil', 'https://images-na.ssl-images-amazon.com/images/I/71lrgj69NQL._SY679_.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Vanilla Extract', 'https://images.samsclubresources.com/is/image/samsclub/0005210003021_A?$img_size_380x380$');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Caraway Seeds', 'https://target.scene7.com/is/image/Target/GUEST_f3302b7e-57c4-4ecf-a95c-3fee66ee5205?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Dried Oregano', 'https://www.indiameetsindia.org/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/0/5/053kidahe001.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Garlic Salt', 'https://target.scene7.com/is/image/Target/GUEST_529a21c7-1777-4fa4-97d7-b8cf893ffbbd');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Ground Black Pepper', 'https://target.scene7.com/is/image/Target/GUEST_8f56133a-a671-4150-962e-95dcc9f64c5c?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Red Pepper Flakes', 'https://scontent.harristeeter.com/legacy/productimagesroot/DJ/8/481398.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Ground Beef', 'https://i5.walmartimages.com/asr/571536d7-21f8-47ac-91ad-e3aece147fbe_3.2c17603139a9a6193a87c5ddae92f385.jpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Parmesan Cheese', 'https://pics.drugstore.com/prodimg/413433/450.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Shredded Mozzarella Cheese', 'https://d1bjorv296jxfn.cloudfront.net/s3fs-public/products/product-packaging/40055_SH_Mzzrll_8oz_Fn.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Tomato Sauce', 'https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/0/0/00817301_1.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Pepperoni Slices', 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_b032a98f-4824-4174-846a-70425de3ab20.JPG');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Sweet Italian Sausage', 'https://www.premiofoods.com/content/uploads/2017/04/Sweet-Italian-Sausage-crop1.png');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Onion', 'https://s3.amazonaws.com/cdn.dixondalefarms.com/images/popup/TWTexasEarlyWhite.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Garlic Cloves', 'https://d2droglu4qf8st.cloudfront.net/2016/03/268082/recipe-16282_Category-CategoryPageDefault_ID-1515170.jpg?v=1515170');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Crushed Tomatoes', 'https://target.scene7.com/is/image/Target/GUEST_377a75b1-4b74-410c-918b-28ad14ddbab7?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Tomato Paste', 'https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/0/0/00817203.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Dried Basil Leaves', 'https://target.scene7.com/is/image/Target/GUEST_66f65393-8b21-400f-84e8-a7b578ca0404?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Fennel Seed', 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_e7e32003-5d1d-4d40-a515-8c1cc2369c73.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Italian Seasoning', 'https://target.scene7.com/is/image/Target/GUEST_ac178f76-cf1d-400f-b4df-3ee041df0696?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Fresh Parsley', 'https://media.glamour.com/photos/5696705ceaefd309768df4aa/master/w_1280,c_limit/home-2014-09-convert-fresh-herbs-to-dried-0910-main.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Lasagna Noodles', 'https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/103/176/484082555.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Ricotta Cheese', 'https://www.shopcaputos.com/store/Assets/ProductImages/00074030102850.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Parmigiano-Reggiano', 'https://www.wegmans.com/content/dam/wegmans/products/614/332614.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Spaghetti', 'https://www.cheatsheet.com/wp-content/uploads/2014/09/iStock_000005748326_Small.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Apple', 'http://usapple.org/wp-content/uploads/2016/01/ThinkstockPhotos-177437649.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Nectarine', 'https://images.samsclubresources.com/is/image/samsclub/0003338332316_A?$img_size_380x380$');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Celery', 'http://www.gosocial.co/wp-content/uploads/2017/07/celery-1.jpg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Dried Cranberries', 'https://target.scene7.com/is/image/Target/GUEST_c5489773-70a4-43ca-a019-3f2581343acf?wid=488&hei=488&fmt=pjpeg');
+
+insert into ingredients (ingredientName, ingredientImage)
+values ('Lemon Yogurt', 'https://pics.drugstore.com/prodimg/467965/900.jpg');
