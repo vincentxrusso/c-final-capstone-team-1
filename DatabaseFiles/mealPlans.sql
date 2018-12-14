a5f70d1b9909b30415dcf9701c3ce1cdaabe9898
Adam Wolfe <mlbfan2@gmail.com>
2018-12-14 10:38:43 -0500

    changed meal plan view and database

--- DatabaseFiles/mealPlans.sql
+++ DatabaseFiles/mealPlans.sql
@@ -1,31 +1,31 @@
 use MealPlanner;
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 1', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 1', 'https://image.flaticon.com/icons/svg/93/93104.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 2', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 2', 'https://image.flaticon.com/icons/svg/18/18353.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 3', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 3', 'https://image.flaticon.com/icons/svg/93/93109.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 4', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 4', 'https://image.flaticon.com/icons/svg/70/70576.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 5', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 5', 'https://image.flaticon.com/icons/svg/18/18515.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 6', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 6', 'https://image.flaticon.com/icons/svg/18/18120.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 7', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 7', 'https://image.flaticon.com/icons/svg/18/18536.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 8', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 8', 'https://image.flaticon.com/icons/svg/93/93079.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 9', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 9', 'https://image.flaticon.com/icons/svg/93/93085.svg');
 
 insert into mealPlans (mealPlanName, mealPlanImage)
-values ('Meal Plan 10', 'https://images.all-free-download.com/images/graphiclarge/vector_cartoon_food_287114.jpg');
+values ('Meal Plan 10', 'https://image.flaticon.com/icons/svg/93/93091.svg');
